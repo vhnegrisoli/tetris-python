40b0919c8235c31164a070d1500194d8e9bad84d
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2019-04-11 19:12:49 -0300

    Define dimensões

--- tetris3.py
+++ tetris3.py
@@ -1,10 +1,19 @@
 from tkinter import *
 
+# Dimensões do jogo
+quadradoLado = 20
+qtdQuadradosAltura = 20
+qtdQuadradosLargura = 10
+largura = quadradoLado * qtdQuadradosLargura
+altura = quadradoLado * qtdQuadradosAltura
+
+
 class Tetris:
 
     def __init__(self):
         self.window = Tk()
-        self.canvas = Canvas(self.window, width=largura, height=largura, bg='black')
+        self.canvas = Canvas(self.window, width=largura,
+                             height=largura, bg='black')
         self.canvas.pack
 
     def run(self):
@@ -12,5 +21,7 @@ class Tetris:
             self.canvas.after(50)
             self.window.update_iddletasks()
             self.window.update()
+
+
 g = Game()
 g.run()
